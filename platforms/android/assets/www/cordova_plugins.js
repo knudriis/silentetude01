31b81afac306df8d0d739b07ffa08f06aabb6481
knudriis <knudriis@gmail.com>
2014-03-15 18:02:07 +0100

    Slettede lowlevelaudioapi

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,17 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/com.rjfun.cordova.plugin.lowlatencyaudio/www/LowLatencyAudio.js",
-        "id": "com.rjfun.cordova.plugin.lowlatencyaudio.LowLatencyAudio",
-        "clobbers": [
-            "window.plugins.LowLatencyAudio"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "com.rjfun.cordova.plugin.lowlatencyaudio": "1.0"
-}
+{}
 // BOTTOM OF METADATA
 });
