6341ff9e5d61fece1fef004a1e8265a9d8c72ef9
knudriis <knudriis@gmail.com>
2014-03-15 09:56:38 +0100

    Tilføjede Lowlevelaudioapi

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,7 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/com.rjfun.cordova.plugin.lowlatencyaudio/www/LowLatencyAudio.js",
+        "id": "com.rjfun.cordova.plugin.lowlatencyaudio.LowLatencyAudio",
+        "clobbers": [
+            "window.plugins.LowLatencyAudio"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "com.rjfun.cordova.plugin.lowlatencyaudio": "1.0"
+}
 // BOTTOM OF METADATA
 });
